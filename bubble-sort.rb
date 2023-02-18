cac0fb214c859b41c146d911e137e6a691e3cd32
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-02-18 22:37:42 +0000

    Add comments

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,8 +1,12 @@
 def bubble_sort(array)
+  #Loop through the array a number of N-1 times
   (array.length - 1).times do
+  #For each current number, check if next one is bigger
     array.each_with_index do |num, index|
+  #Prevents error in case the bubble tries to leave array
       if index == array.length - 1
         break
+  #If current num is bigger, switch the values
       elsif num > array[index+1]
         array[index] = array[index+1]
         array[index+1] = num
