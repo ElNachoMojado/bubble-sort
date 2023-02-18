766e87244c0c5bad5006e2f9e8f95ee81477945f
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-02-18 22:33:53 +0000

    Add bubble_sort method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,2 +1,20 @@
-array = Array.new(20) {rand(40)}
-puts "#{array}"
+def bubble_sort(array)
+  (array.length - 1).times do
+    array.each_with_index do |num, index|
+      if index == array.length - 1
+        break
+      elsif num > array[index+1]
+        array[index] = array[index+1]
+        array[index+1] = num
+      end
+    end
+  end
+  array
+end
+
+puts "How big is your numbers array?"
+array_size = gets.to_i
+
+array = Array.new(array_size) {rand(100)}
+puts "Before: #{array}"
+puts "After: #{bubble_sort(array)}"
